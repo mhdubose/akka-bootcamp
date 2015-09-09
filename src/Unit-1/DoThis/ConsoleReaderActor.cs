5a877edf38c739332c776f69ab7af4f510535807
Michael DuBose <mhdubose@gmail.com>
2015-09-09 11:18:54 -0700

    Completed Lesson 2

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -4,13 +4,15 @@ using Akka.Actor;
 namespace WinTail
 {
     /// <summary>
-    /// Actor responsible for reading FROM the console. 
-    /// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
+    ///     Actor responsible for reading FROM the console.
+    ///     Also responsible for calling <see cref="ActorSystem.Shutdown" />.
     /// </summary>
-    class ConsoleReaderActor : UntypedActor
+    internal class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
-        private IActorRef _consoleWriterActor;
+        public const string StartCommand = "start";
+
+        private readonly IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
         {
@@ -19,23 +21,67 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
+                DoPrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
             }
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-            _consoleWriterActor.Tell(read);
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
-            Self.Tell("continue");
+            GetAndValidateInput();
         }
 
+        #region Internal methods
+        /// <summary>
+        /// Reads input from the console, validates it, then signals appropriate response
+        /// (continue processing, error, success, etc.).
+        /// </summary>
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message))
+            {
+                Self.Tell(new Messages.NullInputError("No input received."));
+            }
+            else if (string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                Context.System.Shutdown();
+            }
+            else
+            {
+                var valid = IsValid(message);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid"));
+
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Validates <see cref="message"/>.
+        /// Currently says messages are valid if contain even number of characters.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static bool IsValid(string message)
+        {
+            return message.Length%2 == 0;
+        }
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+        #endregion
     }
 }
